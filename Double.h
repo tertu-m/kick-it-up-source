e14451f220f1b8255582dc468dd147c5be4dbee6
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2011-07-28 01:04:13 -0500

    fix the stupid macro definitions in Double.h

--- Double.h
+++ Double.h
@@ -1,18 +1,18 @@
 #ifndef __DOUBLE__INCLUDE__
 #define __DOUBLE__INCLUDE__
 
-#define	ZONE_U						35+8-8
-#define	ZONE_D						78-7+7
+#define	ZONE_U						35
+#define	ZONE_D						78
 
-#define	PERFECT_ZONE_U				50+8-8
-#define	GREAT_ZONE_U				45+8-8
-#define	GOOD_ZONE_U					40+8-8
-#define	BAD_ZONE_U					35+8-8
+#define	PERFECT_ZONE_U				50
+#define	GREAT_ZONE_U				45
+#define	GOOD_ZONE_U					40
+#define	BAD_ZONE_U					35
 
-#define	PERFECT_ZONE_D				60-7+7
-#define	GREAT_ZONE_D				68-7+7
-#define	GOOD_ZONE_D					73-7+7
-#define	BAD_ZONE_D					78-7+7
+#define	PERFECT_ZONE_D				60
+#define	GREAT_ZONE_D				68
+#define	GOOD_ZONE_D					73
+#define	BAD_ZONE_D					78
 
 
 void KIU_STAGE_DOUBLE(void);
